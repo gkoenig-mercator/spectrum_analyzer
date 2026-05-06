74789e17c80d4a2e02e241c6a45b7d3532a4f719
gkoenig <gkoenig@mercator-ocean.fr>
2026-05-06 09:49:03 +0000

    Move functions from the main script into cli and config

---
+++ utils/cli.py
@@ -0,0 +1,31 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Compute and plot Fourier spectrum of ocean tracers."
+    )
+    parser.add_argument(
+        "--tracer",
+        type=str,
+        default="o2",
+        choices=list(TRACER_CONFIG.keys()),
+        help=f"Tracer variable to analyse. Choices: {list(TRACER_CONFIG.keys())}",
+    )
+    parser.add_argument(
+        "--start", type=str, default="2026-05-07T00:00:00",
+        help="Start datetime (ISO format)"
+    )
+    parser.add_argument(
+        "--end", type=str, default="2026-05-07T00:00:00",
+        help="End datetime (ISO format)"
+    )
+    parser.add_argument(
+        "--depth", type=float, default=2533.336181640625,
+        help="Target depth in metres"
+    )
+    parser.add_argument(
+        "--save-dir", type=str, default=".",
+        help="Directory to save output figures"
+    )
+    return parser.parse_args()
---
+++ utils/config.py
@@ -0,0 +1,22 @@
+TRACER_CONFIG = {
+    "o2": {
+        "dataset_id": "cmems_mod_glo_bgc-bio_anfc_0.25deg_P1D-m",
+        "long_name": "Dissolved Oxygen",
+        "units": "mmol/m³",
+    },
+    "no3": {
+        "dataset_id": "cmems_mod_glo_bgc-nut_anfc_0.25deg_P1D-m",
+        "long_name": "Nitrate",
+        "units": "mmol/m³",
+    },
+    "chl": {
+        "dataset_id": "cmems_mod_glo_bgc-optics_anfc_0.25deg_P1D-m",
+        "long_name": "Chlorophyll",
+        "units": "mg/m³",
+    },
+    "fe": {
+        "dataset_id": "cmems_mod_glo_bgc-bio_anfc_0.25deg_P1D-m",
+        "long_name": "Iron",
+        "units": "mmol/m³",
+    },
+}
