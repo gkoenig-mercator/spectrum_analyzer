1c9a3f813fd04b96d498d652e856489cb2d30585
gkoenig <gkoenig@mercator-ocean.fr>
2026-05-06 09:52:51 +0000

    Export the data_import function in the utils folder

---
+++ utils/data_import.py
@@ -0,0 +1,21 @@
+from utils.config import TRACER_CONFIG
+import copernicusmarine
+
+def load_tracer_data(args) -> np.ndarray:
+    cfg = TRACER_CONFIG[args.tracer]
+
+    ds = copernicusmarine.subset(
+        dataset_id=cfg["dataset_id"],
+        variables=[args.tracer],
+        minimum_longitude=-60.830714,
+        maximum_longitude=-41.984272,
+        minimum_latitude=21.906553,
+        maximum_latitude=35.717104,
+        start_datetime=args.start,
+        end_datetime=args.end,
+        minimum_depth=args.depth,
+        maximum_depth=args.depth,
+    )
+
+    field = ds[args.tracer].isel(time=0, depth=0).values
+    return field
